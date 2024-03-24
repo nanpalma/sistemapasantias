dc83e66e4050ab9ad300fde45cf88bf1b6268204
pedro.rodriguez4 <pedro.rodriguez@datia.co>
2024-03-24 18:05:36 -0400

    Cambios numero 2

---
+++ resources/lang/vendor/laravelusers/en/app.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel Users app.blade.php language lines
+    |--------------------------------------------------------------------------
+    */
+
+    'nav' => [
+        'toggle-nav-alt'    => 'Toggle Navigation',
+        'login'             => 'Login',
+        'register'          => 'Register',
+        'users'             => 'Users',
+        'logout'            => 'Logout',
+        'roles'             => 'Roles',
+    ],
+
+];
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -103,8 +103,8 @@
             <div class="auth-logo">
                 <a href="index.html"><img src="{{ asset('assets_use/compiled/svg/logo.svg') }}" alt="Logo"></a>
             </div>
-            <h1 class="auth-title">Log in.</h1>
-            <p class="auth-subtitle mb-5">Log in with your data that you entered during registration.</p>
+            <h1 class="auth-title">Iniciar Sesión</h1>
+            <p class="auth-subtitle mb-5">Inicie sesión con los datos que ingresó durante el registro.</p>
 
             <form method="POST" action="{{ route('login') }}">
               @csrf
@@ -113,38 +113,37 @@
                     <div class="form-control-icon">
                         <i class="bi bi-person"></i>
                     </div>
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                 </div>
-                @error('email')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
                 <div class="form-group position-relative has-icon-left mb-4">
                     <input id="password" type="password" placeholder="Ingrese su contraseña" class="form-control form-control-xl @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                     <div class="form-control-icon">
                         <i class="bi bi-shield-lock"></i>
                     </div>
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                 </div>
 
-                @error('password')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
                 <div class="form-check form-check-lg d-flex align-items-end">
                     <input class="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault">
                     <label class="form-check-label text-gray-600" for="flexCheckDefault">
-                        Keep me logged in
+                       Mantenme conectado
                     </label>
                 </div>
                 <button type="submit" class="btn btn-primary btn-block btn-lg shadow-lg mt-5">
-                    {{ __('Login') }}
+                    Entrar
                 </button>
             </form>
             <div class="text-center mt-5 text-lg fs-4">
-                <p class="text-gray-600">Don't have an account? <a href="{{ route('register') }}" class="font-bold">Sign
-                        up</a>.</p>
-                <p><a class="font-bold" href="{{ route('password.request') }}">Forgot password?</a>.</p>
+                <p class="text-gray-600">¿No tienes una cuenta? <a href="{{ route('register') }}" class="font-bold">Registrate aquí</a>.</p>
+                <p><a class="font-bold" href="{{ route('password.request') }}">¿Has olvidado tu contraseña?</a>.</p>
             </div>
         </div>
     </div>
